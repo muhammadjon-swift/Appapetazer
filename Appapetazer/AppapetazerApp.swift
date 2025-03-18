0b631bdd52ee85bf5e213bae8cb340647710e8b2
muhammadjon-swift <muhammadjonon.jon@gmail.com>
2025-03-18 16:51:40 +0100

    Initial Commit

---
+++ Appapetazer/AppapetazerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppapetazerApp.swift
+//  Appapetazer
+//
+//  Created by Muhammadjon on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AppapetazerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appapetazer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appapetazer
+//
+//  Created by Muhammadjon on 18/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
